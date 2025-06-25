a282cb6a591172602b0592cf64dc7e5ef30116f9
Yeison <yeison_fexa@hotmail.com>
2025-06-24 21:41:06 -0400

    Generacion de graficas con python

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+
+# Leer archivo Excel
 
 archivo_excel = "datos/datos_base.xlsx"
+data_hoja = pd.read_excel(archivo_excel, sheet_name="Hoja5")
+print(data_hoja)
 
-data_hoja_1 = pd.read_excel(archivo_excel, sheet_name="Hoja1")
+# Grafica de ejemplo
+eje_y = [2, 3, 5, 7, 11]
+eje_x = ["A", "B", "C", "D", "E"]
+fig, ax = plt.subplots()
+ax.bar(eje_x, eje_y, color="blue", label="Datos de ejemplo")
+fig.savefig("generados/grafica_ejemplo.png", bbox_inches="tight")
 
-print(data_hoja_1)
+# Graficar datos de la hoja 5
+eje_x = data_hoja.iloc[:, 0]  # Asumiendo que la primera columna es el eje X
+eje_y = data_hoja.iloc[:, 1]  # Asumiendo que la segunda columna es el eje Y
+fig, ax = plt.subplots()
+ax.bar(eje_x, eje_y, color="green", label="Datos de Hoja5")
+ax.set_xlabel("Eje X")
+ax.set_ylabel("Eje Y")
+ax.set_xticklabels(eje_x, rotation=45)
+fig.savefig("generados/grafica_hoja5.png", bbox_inches="tight")
